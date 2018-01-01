6ca304ddaf0331a9fc8c86e134713ea0d58b5390
0x9fff00 <0x9fff00+git@protonmail.ch>
2018-01-01 21:53:36 +0100

    Rename curseforge.py to curseforge_minecraft.py

--- test.py
+++ test.py
@@ -12,8 +12,8 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from downloaders import curseforge, mediafire
-print(curseforge.get_data('advanced-rocketry', '1.10.2', 'Beta'))
-print(curseforge.get_data('rftools', '1.10.2', 'Beta'))
-print(curseforge.get_url('rftools', '1.7.10', 'Release'))
+from downloaders import curseforge_minecraft, mediafire
+print(curseforge_minecraft.get_data('advanced-rocketry', '1.10.2', 'Beta'))
+print(curseforge_minecraft.get_data('rftools', '1.10.2', 'Beta'))
+print(curseforge_minecraft.get_url('rftools', '1.7.10', 'Release'))
 print(mediafire.get_url('http://www.mediafire.com/file/2czafa60rh4ajhj/mcp908.zip'))
